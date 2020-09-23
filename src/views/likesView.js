5563074b14303b25462ba2601105e0328da80fc6
fpovina2004 <57961919+fpovina2004@users.noreply.github.com>
2020-09-23 14:42:28 -0300

    UI changes and cleanup :nail_care:

--- src/views/likesView.js
+++ src/views/likesView.js
@@ -3,7 +3,7 @@ import { elements } from './base.js';
 export const renderListItem = (activity) => {
     const html = `
             <li>
-              <a href="#${activity.key}" class="border-white border-2 flex flex-row px-6 py-4 justify-evenly">
+              <a href="#${activity.key}" class="border-primary-300 border-2 flex flex-row px-6 py-4 justify-evenly">
                 <img src="img/${activity.type}.svg" heigh="48" width="48">
                 <h4 class="text-base p-2">${activity.title}</h4>
               </a>
